d8e90eed047a5de42dcd803dcf5e1531ba2402c3
UNBRAKING THINGS <code.pbail@gmail.com>
2025-05-18 20:06:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "7d2c9a5628814e1430b30a1f0dc0165b")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8017499587:AAHwx8VdtQsf83ujqKCF8xnByuT-8gLZrYk") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://xefiw96298:TVM8AI1hG5dA2kSc@cluster0.avacy9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://gajogim551:pkF65rfEWB2yrCko@cluster0.pysm9th.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "team_pbail")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '7641491740').split()]
 
