2b57578d6b5923df32fd47472cac97265ea6f6b1
UNBRAKING THINGS <code.pbail@gmail.com>
2025-05-25 18:26:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,9 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "26614080")
-    API_HASH = os.environ.get("API_HASH", "7d2c9a5628814e1430b30a1f0dc0165b")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8017499587:AAHwx8VdtQsf83ujqKCF8xnByuT-8gLZrYk")
+    API_ID = os.environ.get("API_ID", "")
+    API_HASH = os.environ.get("API_HASH", ")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", ")
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot")
     # Add error handling for DB_URL
     DB_URL = os.environ.get("DB_URL")
